49e8573058579ae5dba82c46d72f02e0dd4c33a7
Sohaib Ibenhajene <r0883629@student.thomasmore.be>
2023-10-14 15:01:49 +0200

    Transfer-service base code finished

--- transfer-service/src/main/java/fact/it/transferservice/controller/TransferController.java
+++ transfer-service/src/main/java/fact/it/transferservice/controller/TransferController.java
@@ -1,11 +1,29 @@
 package fact.it.transferservice.controller;
 
+import fact.it.transferservice.dto.TransferRequest;
+import fact.it.transferservice.dto.TransferResponse;
+import fact.it.transferservice.service.TransferService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transfer")
 @RequiredArgsConstructor
 public class TransferController {
+    private final TransferService transferService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeTransfer(@RequestBody TransferRequest transferRequest) {
+        transferService.placeTransfer(transferRequest);
+        return "Transfer completed successfully";
+    }
+
+    @GetMapping
+    public List<TransferResponse> getAllTransfers() {
+        return transferService.getAllTransfers();
+    }
 }
